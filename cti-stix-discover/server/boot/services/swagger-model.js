fb85b716f9ed986981ab37f308c43ff1d960d2c5
exceptionfactory <exceptionfactory@users.noreply.github.com>
2016-10-14 14:42:02 -0400

    Aded getModelProperties to Swagger Model Service

--- cti-stix-discover/server/boot/services/swagger-model.js
+++ cti-stix-discover/server/boot/services/swagger-model.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const lodash = require('lodash');
+
 /**
  * Swagger Model Service creates Loopback Models based on Swagger Specification
  *
@@ -10,7 +12,7 @@ module.exports = class SwaggerModelService {
    *
    */
   constructor() {
-
+    this.datePattern = new RegExp(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/);
   }
 
   /**
@@ -34,7 +36,7 @@ module.exports = class SwaggerModelService {
             const findOperationPath = findOperation.path;
             console.log(`Located Find Operation [${findOperationId}] with Path [${findOperationPath}]`);
 
-            const modelProperties = {};
+            const modelProperties = this.getModelProperties(findOperation);
             const modelOptions = this.getModelOptions(findOperation);
             const model = dataSource.createModel(tag, modelProperties, modelOptions);
             this.disableRemoteMethods(model);
@@ -55,6 +57,65 @@ module.exports = class SwaggerModelService {
    */
   getSwaggerClient(dataSource) {
     return dataSource.connector.client;
+  }
+
+  /**
+   * Get Model Properties for specified operation
+   *
+   * @param {Object} operation Operation Object
+   * @return {Object} Properties Object
+   */
+  getModelProperties(operation) {
+    const modelProperties = {};
+
+    const response = operation.successResponse['200'];
+    if (response) {
+      const sampleValue = response.getSampleValue();
+      if (sampleValue.data) {
+        if (sampleValue.data.length) {
+          const resourceObject = sampleValue.data[0];
+          if (resourceObject.attributes) {
+            for (const key in resourceObject.attributes) {
+              const attribute = resourceObject.attributes[key];
+              const attributeType = this.getAttributeType(attribute);
+
+              modelProperties[key] = {
+                type: attributeType
+              };
+            }
+          }
+        }
+      }
+    }
+
+    return modelProperties;
+  }
+
+  /**
+   * Get Attribute Type
+   *
+   * @param {Object} attribute Attribute
+   * @return {string} Attribute Type
+   */
+  getAttributeType(attribute) {
+    let attributeType = 'string';
+
+    if (lodash.isArray(attribute)) {
+      attributeType = 'array';
+    } else if (lodash.isBoolean(attribute)) {
+      attributeType = 'boolean';
+    } else if (lodash.isPlainObject(attribute)) {
+      attributeType = 'object';
+    } else if (lodash.isNumber(attribute)) {
+      attributeType = 'number';
+    } else {
+      const dateMatcher = this.datePattern.exec(attribute);
+      if (dateMatcher) {
+        attributeType = 'date';
+      }
+    }
+
+    return attributeType;
   }
 
   /**
